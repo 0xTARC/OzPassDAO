ef53503b0bbcb30bb1783d272d6f085382fb8106
0xTARC <0xtarc@protonmail.com>
2022-10-06 15:26:05 -0700

    tried buying on opensea with forked mainnet but eth wasnt showing up godamn

--- scripts/run-testnet.sh
+++ scripts/run-testnet.sh
@@ -9,4 +9,5 @@ yarn run ganache \
   --fork.url="https://mainnet.infura.io/v3/$INFURA_KEY" \
   --chain.chainId=1 \
   --miner.defaultGasPrice=2000000000 \
-  --unlock="0x2F5B5F7771aCdc698Bce5FEb53C40084939D7b60" # oz holder, pass id 145
+  --unlock="0x2F5B5F7771aCdc698Bce5FEb53C40084939D7b60" \ # oz holder, pass id 145
+  --unlock="0x28C6c06298d514Db089934071355E5743bf21d60" # binance
--- scripts/transfer_localhost_tokens.js
+++ scripts/transfer_localhost_tokens.js
@@ -11,6 +11,22 @@ const fs = require("fs");
     "http://localhost:8545"
   );
   try {
+    // Steal ETH From Binance wallet
+    const ethBalBefore = await provider.getBalance("<your_address>")
+    console.log('eth bal before: ', ethBalBefore.toString())
+
+    // steal eth from binance
+    const binanceSigner = provider.getSigner(
+      "0x28C6c06298d514Db089934071355E5743bf21d60"
+    );
+    await binanceSigner.sendTransaction({
+      to: "<your_address>",
+      value: ethers.utils.parseEther("100.0")
+    });
+    const ethBalAfter = await provider.getBalance("<your_address>")
+    console.log('eth bal after: ', ethBalAfter.toString())
+
+    // Steal oz pass
     // tokenid 145 https://etherscan.io/token/0x0599699bbfc3a92589ad249607f7265c08a1fb61?a=145
     // held by 0x2F5...b60
     const ozHolderSigner = provider.getSigner(
@@ -19,8 +35,8 @@ const fs = require("fs");
     const mainnetOzPassAddress = "0x0599699BBFC3a92589AD249607f7265c08A1FB61";
     const ozAbi = JSON.parse(fs.readFileSync("./src/abis/Oz.json"));
     const oz = new ethers.Contract(mainnetOzPassAddress, ozAbi, ozHolderSigner);
-    const from = ozHolderSigner.address;
-    const to = "0x84c298CDfA2a4F1758c5989692c6C3c91E9Fd024";
+    // const from = ozHolderSigner.address;
+    const to = "<your_address>";
     const tokenId = 145;
     const amount = 1;
     try {
@@ -31,9 +47,9 @@ const fs = require("fs");
         amount,
       );
       const txResult = await tx.wait(1)
-      console.log('tx result: ', txResult)
+      // console.log('tx result: ', txResult)
 
-      const balance = await oz.balanceOfBatch(['0x84c298CDfA2a4F1758c5989692c6C3c91E9Fd024'], [145])
+      const balance = await oz.balanceOfBatch(['<your_address>'], [145])
       console.log('your balance of oz nft id 145:', balance.toString())
     } catch (e) {
       console.error("\nERROR:\n");
